9f381712c0b60c11078a6d8fedcdc4bd43b5d4ab
bendevlin18 <bdev1238@gmail.com>
2021-03-16 17:24:04 -0400

    Adding data combination

---
+++ combine_mupet_output.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import pandas as pd
+import os
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+
+
+## gathering files
+meta_filename = filedialog.askopenfilename(title = 'Select the metadata file')
+df_filename = filedialog.askopenfilename(title = 'Select the batch output file')
+
+## opening files
+meta = pd.read_csv(meta_filename)[['Pup Number', 'File legend']]
+df = pd.read_csv(df_filename)
+
+## cleaning output dataframe
+age = ['P8'] * len(df)
+file_legend = df['filename'].copy()
+
+for i in range(len(df['filename'])):
+    if 'P15' in df['filename'][i]:
+        age[i] = 'P15'
+        file_legend[i] = '_'.join(df['filename'][i].split('_')[0:-2] + [df['filename'][i].split('_')[-1]])
+        
+df['age'] = age
+df['file_legend'] = file_legend
+
+## making sure the keys are case-consistent
+meta['File legend'] = meta['File legend'].str.lower()
+df['file_legend'] = df['file_legend'].str.lower()
+
+## merging the files together
+output = df.set_index('file_legend').join(meta.set_index('File legend'))
+
+## asking for output directory and outputting the combined file
+output_dir = filedialog.askdirectory(title = 'Select the output directory')
+output.to_csv(os.path.join(output_dir, 'combined_data.csv'))
+
+print('All Done! You can find the processed output here:'+ output_dir + '/combined_data.csv')
